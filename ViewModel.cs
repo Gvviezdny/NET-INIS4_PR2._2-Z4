16a840eafba9bca7d15d4d8f0dd78ec9da58b7ce
Marcin <Marcin@DESKTOP-STSUK8D>
2022-06-28 18:11:58 +0200

    Dodaj pliki projektów.

---
+++ ViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace NET_INIS4_PR2._2_Z4
+{
+    public class ViewModel
+    {
+        public XmlDataProvider XmlData { get; set; }
+
+        public ViewModel()
+        {
+            XmlData = new XmlDataProvider();
+            XmlData.Source = new Uri(@"C:\Users\dudekpob\Desktop\Projekty\zadania4_net2\4\NET-INIS4_PR2._2-Z5-main\NET-INIS4_PR2._2-Z5-main\xml\biding.xml");
+            XmlData.XPath = "node";
+        }
+    }
+}
